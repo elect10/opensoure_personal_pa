80a744598f2f0f4909e2d4b0ae2172120994c8f0
elect10 <wlgnsakswp@naver.com>
2024-06-04 22:01:16 +0900

    make radio button for position choose

---
+++ my-app/src/routes/components/PositionRadioButtons.jsx
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+import useProjectStore from '.../store/useProjectStore';
+
+const RadioButtonLabel = styled.label`
+  display: block;
+  margin: 10px;
+  margin-left: 0px;
+`;
+
+const RadioButton = styled.input`
+  margin: 10px;
+`;
+
+const PositionRadioButtons = ({ selectedPosition, setSelectedPosition }) => {
+  const { projectData } = useProjectStore();
+
+  const handlePositionChange = (event) => {
+    // let positionValue;
+
+    // switch (event.target.value) {
+    //   case 'Front-end':
+    //     positionValue = 0;
+    //     break;
+    //   case 'Back-end':
+    //     positionValue = 1;
+    //     break;
+    //   case 'Designer':
+    //     positionValue = 2;
+    //     break;
+    //   default:
+    //     console.error('Position Selection Error');
+    //     return;
+    // }
+
+    // setSelectedPosition(positionValue);
+
+    setSelectedPosition(event.target.value);
+  };
+
+  return (
+    <>
+      {projectData && projectData.front_req > 0 && (
+        <RadioButtonLabel>
+          <RadioButton
+            type="radio"
+            name="position"
+            value="Front-end"
+            checked={selectedPosition === "Front-end"}
+            onChange={handlePositionChange}
+          />
+          Front-end
+        </RadioButtonLabel>
+      )}
+      {projectData && projectData.back_req > 0 && (
+        <RadioButtonLabel>
+          <RadioButton
+            type="radio"
+            name="position"
+            value="Back-end"
+            checked={selectedPosition === "Back-end"}
+            onChange={handlePositionChange}
+          />
+          Back-end
+        </RadioButtonLabel>
+      )}
+      {projectData && projectData.design_req > 0 && (
+        <RadioButtonLabel>
+          <RadioButton
+            type="radio"
+            name="position"
+            value="Designer"
+            checked={selectedPosition === "Designer"}
+            onChange={handlePositionChange}
+          />
+          Designer
+        </RadioButtonLabel>
+      )}
+    </>
+  );
+};
+
+export default PositionRadioButtons;
