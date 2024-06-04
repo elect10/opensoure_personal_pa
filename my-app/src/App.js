15ba9eb289d30eaba480e8589221bbee2160f941
elect10 <wlgnsakswp@naver.com>
2024-06-04 18:43:30 +0900

    fix app.js for routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,6 +14,9 @@ import Login from "./routes/Login";
 import Main from "./routes/Main";
 import Makepost from "./routes/Makepost";
 import SignUp from "./routes/SignUp";
+import Scrab from "./routes/Scrab";
+import ProfilePage from "./routes/ProfilePage";
+
 
 function App() {
   return (
@@ -24,6 +27,9 @@ function App() {
         <Route path="/main" element={<Main />} />
         <Route path="/makepost" element={<Makepost />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/scrab" element={<Scrab />} />
+        <Route path="/profile" element={<ProfilePage />} />
+
       </Routes>
     </Router>
   );
